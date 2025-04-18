0d3b3c5cfb253b02be68f69c3c04863802285679
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2025-04-18 08:08:28 -0400

    Update pickup.js

--- js/pickup.js
+++ js/pickup.js
@@ -6,40 +6,81 @@ class Pickup {
         this.width = 20;
         this.height = 20;
         this.type = type;
+        this.rotation = 0;
+        this.bobOffset = 0;
+        this.bobDirection = 1;
     }
     
     update(deltaTime) {
         this.y += this.game.gameSpeed;
+        
+        // Bobbing animation
+        this.bobOffset += 0.05 * this.bobDirection;
+        if (Math.abs(this.bobOffset) > 3) {
+            this.bobDirection *= -1;
+        }
+        
+        // Rotation animation
+        this.rotation += 0.02;
     }
     
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x + this.width/2, this.y + this.height/2 + this.bobOffset);
+        ctx.rotate(this.rotation);
+        
+        // Draw different types of pickups
         switch (this.type) {
             case 'life':
-                ctx.fillStyle = '#ff69b4'; // Pink
+                ctx.fillStyle = '#ff69b4';
+                this.drawHeart(ctx);
                 break;
             case 'speed':
-                ctx.fillStyle = '#ffff00'; // Yellow
+                ctx.fillStyle = '#ffff00';
+                this.drawLightning(ctx);
                 break;
             case 'points':
-                ctx.fillStyle = '#00ffff'; // Cyan
+                ctx.fillStyle = '#00ffff';
+                this.drawStar(ctx);
                 break;
         }
         
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // Glow effect
+        ctx.shadowColor = ctx.fillStyle;
+        ctx.shadowBlur = 10;
         
-        // Draw symbol based on type
-        ctx.fillStyle = 'white';
-        ctx.font = '16px Arial';
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle';
-        
-        let symbol = '';
-        switch (this.type) {
-            case 'life': symbol = '♥'; break;
-            case 'speed': symbol = '⚡'; break;
-            case 'points': symbol = '★'; break;
+        ctx.restore();
+    }
+    
+    drawHeart(ctx) {
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.bezierCurveTo(5, -10, 15, -10, 0, -20);
+        ctx.bezierCurveTo(-15, -10, -5, -10, 0, 0);
+        ctx.fill();
+    }
+    
+    drawLightning(ctx) {
+        ctx.beginPath();
+        ctx.moveTo(0, -10);
+        ctx.lineTo(5, 0);
+        ctx.lineTo(-5, 5);
+        ctx.lineTo(5, 10);
+        ctx.lineTo(0, 0);
+        ctx.lineTo(10, -5);
+        ctx.closePath();
+        ctx.fill();
+    }
+    
+    drawStar(ctx) {
+        ctx.beginPath();
+        for (let i = 0; i < 5; i++) {
+            ctx.lineTo(0, -10);
+            ctx.rotate((Math.PI * 2) / 5);
+            ctx.lineTo(0, -5);
+            ctx.rotate((Math.PI * 2) / 5);
         }
-        
-        ctx.fillText(symbol, this.x + this.width / 2, this.y + this.height / 2);
+        ctx.closePath();
+        ctx.fill();
     }
 }
