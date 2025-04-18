771697963c23efda20a18041a178392a04b26cee
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2025-04-18 08:15:48 -0400

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -5,7 +5,8 @@ class Enemy {
         this.y = y;
         this.width = 30;
         this.height = 30;
-        this.speedX = Math.random() > 0.5 ? 1 : -1;
+        this.speedX = Math.random() > 0.5 ? 2 : -2;
+        this.animationFrame = 0;
     }
     
     update(deltaTime) {
@@ -18,15 +19,34 @@ class Enemy {
         if (this.x <= 0 || this.x + this.width >= this.game.gameWidth) {
             this.speedX *= -1;
         }
+        
+        // Animation
+        this.animationFrame += deltaTime * 0.01;
     }
     
     draw(ctx) {
-        ctx.fillStyle = '#ff0000';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // Body
+        ctx.fillStyle = '#e74c3c';
+        ctx.beginPath();
+        ctx.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
+        ctx.fill();
         
-        // Draw eyes
+        // Eyes - animated
+        const eyeOffset = Math.sin(this.animationFrame) * 2;
         ctx.fillStyle = 'white';
-        ctx.fillRect(this.x + 5, this.y + 5, 5, 5);
-        ctx.fillRect(this.x + 20, this.y + 5, 5, 5);
+        ctx.beginPath();
+        ctx.arc(this.x + this.width/3, this.y + this.height/3 + eyeOffset, 4, 0, Math.PI * 2);
+        ctx.arc(this.x + 2*this.width/3, this.y + this.height/3 + eyeOffset, 4, 0, Math.PI * 2);
+        ctx.fill();
+        
+        // Angry eyebrows
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(this.x + this.width/3 - 5, this.y + this.height/4);
+        ctx.lineTo(this.x + this.width/3 + 5, this.y + this.height/4 - 3);
+        ctx.moveTo(this.x + 2*this.width/3 - 5, this.y + this.height/4 - 3);
+        ctx.lineTo(this.x + 2*this.width/3 + 5, this.y + this.height/4);
+        ctx.stroke();
     }
 }
